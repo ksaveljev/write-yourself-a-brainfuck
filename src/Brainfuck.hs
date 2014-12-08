89274decac213171942d516eca931656cb20681d
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-08 22:15:23 +0200

    Introducing Tape data type

--- src/Brainfuck.hs
+++ src/Brainfuck.hs
@@ -27,6 +27,20 @@ instance Show BrainfuckSource where
         bfToChar LoopR       = ']'
         bfToChar (Comment c) = c
 
+data Tape a = Tape [a] -- Left of the pivot element
+                    a  -- Pivot element
+                   [a] -- Right of the pivot element
+
+emptyTape :: Tape Int
+emptyTape = Tape zeros 0 zeros
+  where
+    zeros = repeat 0
+
+moveRight :: Tape a -> Tape a
+moveRight (Tape ls p (r:rs)) = Tape (p:ls) r rs
+
+moveLeft :: Tape a -> Tape a
+moveLeft (Tape (l:ls) p rs) = Tape ls l (p:rs)
 
 parseBrainfuck :: String -> Either String BrainfuckSource
 parseBrainfuck = checkSyntax . BrainfuckSource . mapMaybe charToBF
